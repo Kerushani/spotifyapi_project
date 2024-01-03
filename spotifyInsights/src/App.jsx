9f307dead6f307adad7acaffdd4b7bc1a37ef58d
Kerushani <kerushanisivaneswaran@gmail.com>
2024-01-03 14:07:34 -0500

    set up firebase auth

--- spotifyInsights/src/App.jsx
+++ spotifyInsights/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx"
+import SignUp from "./pages/SignUp.jsx";
+
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./components/Navbar.jsx";
+
+import * as ROUTES from "./constants/routes.js"
 
 function App() {
-
   return (
-    <div>
-      <h1 className="text-sky-400">Hey</h1>
-      <Button variant="primary">Test button</Button>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path={ROUTES.PROFILE} element={<Profile />} />
+          <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+        </Routes>
+
+        {/* <Button variant="primary">Test button</Button> */}
+
+      </div>
+    </Router>
   );
 }
 
